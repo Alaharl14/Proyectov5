d044aec13ecc03f39d987ba6bbcadce5a422771c
Cesar0023 <113397874+Cesar0023@users.noreply.github.com>
2022-09-26 19:06:05 -0500

    MainTrabajoCesar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CuentaComponent } from './page/cuenta/cuenta.component';
 import { ObjetoComponent } from './page/objeto/objeto.component';
 import { ReporteComponent } from './page/reporte/reporte.component';
+import { DistritoComponent } from './page/distrito/distrito.component';
 import { ObjetoCreaeditaComponent } from './page/objeto/objeto-creaedita/objeto-creaedita.component';
 import { ReporteCreaeditaComponent } from './page/reporte/reporte-creaedita/reporte-creaedita.component';
+import { DistritoCreaeditaComponent } from './page/distrito/distrito-creaedita/distrito-creaedita.component';
 
 const routes: Routes = [{
   path: 'cuenta', component: CuentaComponent, children:[
@@ -18,6 +20,14 @@ const routes: Routes = [{
   path: 'reporte', component: ReporteComponent, children:[
     { path: 'agregar', component: ReporteCreaeditaComponent },
     { path: 'edicion/:id', component: ReporteCreaeditaComponent }
+
+
+  ]
+}, {  
+    path: 'distrito', component: DistritoComponent, children:[
+      { path: 'agregar', component: DistritoCreaeditaComponent },
+      { path: 'edicion/:id', component: DistritoCreaeditaComponent }
+    
   ]
 }];
 
